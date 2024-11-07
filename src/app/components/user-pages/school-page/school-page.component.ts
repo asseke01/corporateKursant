8791466c4657b30ef12fd437043f2bba295c1b9a
abaymtz <abaymtz22@gmail.com>
2024-11-07 15:30:18 +0500

    navbar sidebar burger menu finished

---
+++ src/app/components/user-pages/school-page/school-page.component.ts
@@ -0,0 +1,78 @@
+import {Component, inject, OnInit} from '@angular/core';
+import {UserNavbarComponent} from '../user-navbar/user-navbar.component';
+import {ActivatedRoute} from '@angular/router';
+import {FooterComponent} from '../footer/footer.component';
+import {NgForOf} from '@angular/common';
+import {NewsService} from '../../../services/news-service/news.service';
+import {News} from '../../../../assets/news.interface';
+
+@Component({
+  selector: 'app-school-page',
+  standalone: true,
+  imports: [
+    UserNavbarComponent,
+    FooterComponent,
+    NgForOf
+  ],
+  templateUrl: './school-page.component.html',
+  styleUrl: './school-page.component.css'
+})
+export class SchoolPageComponent implements OnInit {
+  public schoolName: string | null = null;
+
+  constructor(private route: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.schoolName = params.get('schoolName');
+    });
+    console.log(this.schoolName);
+
+    this.getNews();
+  }
+
+  public teacherCards: any = [
+    {
+      name: 'Оралбек Тоқтарбек',
+      title: '“Kursant Kaskelen” Мектеп директоры',
+      text: '“Оқушылар тұлға болып қалыптасудың соңғы қадамында жүргендіктен оларға тек білім беріп қоймай, сол түйгенін\n' +
+        '        дұрыс пайдалануға, жақсы университетке түсуге, дұрыс мамандық таңдауға, өмірінде дұрыс шешім шығаруға үйрету —\n' +
+        '        мақсатымыз.”',
+      image: 'assets/img/teacher.svg',
+    },
+    {
+      name: 'Қадырбай Бексеит',
+      title: '“Kursant Kaskelen” Директор орынбасары',
+      text: '“Бәріміз бірге еліміздің дамуы мен халқымыздың өркендеуіне елеулі үлес қосатынымызға сеніміміз мол:”',
+      image: 'assets/img/teacher2.svg',
+    },
+    {
+      name: 'Оралбек Тоқтарбек',
+      title: '“Kursant Kaskelen” Мектеп директоры',
+      text: '“Оқушылар тұлға болып қалыптасудың соңғы қадамында жүргендіктен оларға тек білім беріп қоймай, сол түйгенін\n' +
+        '        дұрыс пайдалануға, жақсы университетке түсуге, дұрыс мамандық таңдауға, өмірінде дұрыс шешім шығаруға үйрету —\n' +
+        '        мақсатымыз.”',
+      image: 'assets/img/teacher.svg',
+    },
+  ]
+
+
+  private newsService = inject(NewsService);
+
+  public news: News[] = []
+  public showAllNews: boolean = false;
+
+  public getNews(id: number = 3) {
+    this.newsService.getNews(id).subscribe(data => {
+      this.news = data.map((news: News) => ({
+        ...news,
+        poster: `http://127.0.0.1:8000${news.poster}`
+      }));
+    })
+  }
+
+  public toggleShowAllNews() {
+    this.showAllNews = !this.showAllNews;
+  }
+}
--- src/app/components/user-pages/user-navbar/user-navbar.component.ts
+++ src/app/components/user-pages/user-navbar/user-navbar.component.ts
@@ -1,8 +1,9 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {MatFormField, MatLabel} from '@angular/material/form-field';
 import {MatOption, MatSelect, MatSelectTrigger} from '@angular/material/select';
 import {NgForOf, NgIf} from '@angular/common';
 import {MatButton} from '@angular/material/button';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-user-navbar',
@@ -22,10 +23,18 @@ import {MatButton} from '@angular/material/button';
 })
 export class UserNavbarComponent {
 
+  private router = inject(Router);
+
 
   public selectedProject: string | undefined;
   public selectedSchool: string | undefined;
   public schools = ['Алмалыбақ', 'Қаскелең'];
+
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 
   onProjectChange(event: any): void {
     const project = event.value;
@@ -34,6 +43,15 @@ export class UserNavbarComponent {
     } else if (project === 'Курсант') {
       window.open('https://www.kursant.kz/', '_blank');
     }
+  }
+
+  onSchoolChange(event: any): void {
+    const school = event.value;
+    this.router.navigate(['/school', school]); // Navigate to school route with school name as parameter
+  }
+
+  backToMain() {
+    this.router.navigate(['']);
   }
 
 }
